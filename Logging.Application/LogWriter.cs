75eff79853aa50888476134dc5ea2024dae31289
Soheil Karami <sohil_ww@ymail.com>
2016-11-24 01:52:14 +0330

    Add New Project

---
+++ Logging.Application/LogWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LoggingDomianModel;
+using UserOperationLoggingSystemService;
+
+namespace Logging.Application
+{
+    public class LogWriter: ILogWriter
+    {
+        readonly ILogRepository _repository;
+        public LogWriter(ILogRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Write(LogModel model)
+        {
+            //Todo:Add Check Filters
+
+            _repository.Save(model);
+        }
+
+      
+    }
+}
